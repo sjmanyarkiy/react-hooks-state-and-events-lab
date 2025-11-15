f81450d3367b1fb7255c3b0da060f9d90796f009
commit-panda <sjmanyarkiy@gmail.com>
2025-11-15 12:59:22 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use, useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,20 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+
+  const [ isToggled, setToggle ] = useState(false)
+
+  function isDark(){
+    setToggle(!isToggled)
+  }
+
+  const appClass = isToggled ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={isDark}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import items from '../data/items'
 
 function Item({ name, category }) {
+
+  const [ isInCart, setisInCart ] = useState(false)
+
+  function addToCart(){
+
+    setisInCart(!isInCart)
+
+  }
+
+
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{isInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
